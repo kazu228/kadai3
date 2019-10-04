ecddf89fec0aed1eabf5f7668157c2b09ae5a379
kuroneko <kskkm0228@gmail.com>
2019-10-04 12:21:22 +0900

    urlを追加しました。

--- kadai2/polls/views.py
+++ kadai2/polls/views.py
@@ -5,10 +5,9 @@ from django.http import HttpResponse
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." %question_id)
 
-def result(request, question_id):
+def results(request, question_id):
     response = "You're looking at the results of question %s."
     return HttpResponse(response % question_id)
 
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s" %question_id)
-    
