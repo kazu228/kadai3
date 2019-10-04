cca03d3c3a607bfc33b25003f75071f6675f90c6
kuroneko <kskkm0228@gmail.com>
2019-10-04 12:24:20 +0900

    viewを改変

--- kadai2/polls/views.py
+++ kadai2/polls/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.") 
+
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." %question_id)
 
