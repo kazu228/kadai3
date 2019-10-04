0affd31716ce5ebc984b0a93043933ac4e31c015
kuroneko <kskkm0228@gmail.com>
2019-10-04 15:53:19 +0900

    form を完成

--- kadai2/polls/views.py
+++ kadai2/polls/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404
-
-from .models import Question
+from django.urls import reverse
+from .models import Question, Choice
 # Create your views here.
 
 def index(request):
@@ -21,4 +21,16 @@ def results(request, question_id):
     return HttpResponse(response % question_id)
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s" %question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice =   question.choise_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice."
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+
+    return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
