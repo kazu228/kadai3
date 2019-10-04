61983d7cf33f795cd21566949911dee7a22a6b93
kuroneko <kskkm0228@gmail.com>
2019-10-04 16:15:44 +0900

    ok

--- kadai2/polls/views.py
+++ kadai2/polls/views.py
@@ -23,7 +23,7 @@ def results(request, question_id):
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     try:
-        selected_choice =   question.choise_set.get(pk=request.POST['choice'])
+        selected_choice =   question.choice_set.get(pk=request.POST['choice'])
     except (KeyError, Choice.DoesNotExist):
         return render(request, 'polls/detail.html', {
             'question': question,
