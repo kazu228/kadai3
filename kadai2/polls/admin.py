5b813bf46829c972f55e740039466abfc2026e28
kuroneko <kskkm0228@gmail.com>
2019-10-05 15:23:44 +0900

    adminサイトをカスタマイズ

--- kadai2/polls/admin.py
+++ kadai2/polls/admin.py
@@ -2,4 +2,6 @@ from django.contrib import admin
 from .models import Question
 
 # Register your models here.
-admin.site.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ['pub_date', 'question_text']
+admin.site.register(Question, QuestionAdmin)
