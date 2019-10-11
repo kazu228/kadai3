4a71e0bf711a45a6269f13ff686ccf5f5ab0b3af
kuroneko <kskkm0228@gmail.com>
2019-10-11 17:30:24 +0900

    send_mail関数のurlを削除

--- kadai2/app/urls.py
+++ kadai2/app/urls.py
@@ -9,7 +9,6 @@ urlpatterns = [
     path('login/', views.Login.as_view(), name='login'),
     path('logout/', views.Logout.as_view(), name='logout'),
     path('form/', views.form, name='form'),
-    path('mail/', views.send_mail, name='send_mail'),
     path('confirm/', views.confirm, name='confirm'),
     path('signup/', views.signup, name='signup'),
 ]
