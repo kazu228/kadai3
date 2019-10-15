a9f17749161a0ccedae0cd14418e4983b292a60c
kuroneko <kskkm0228@gmail.com>
2019-10-15 17:42:55 +0900

    別のプロジェクトで

--- kadai2/app/forms.py
+++ kadai2/app/forms.py
@@ -1,11 +1,9 @@
 from django.contrib.auth.forms import (
-    AuthenticationForm, UserCreationForm
+    AuthenticationForm
 )
 from django import forms
 from .models import introducer
-from django.contrib.auth import get_user_model
 
-User = get_user_model()
 
 
 class IntroducerForm(forms.ModelForm):
@@ -33,15 +31,3 @@ class LoginForm(AuthenticationForm):
             field.widget.attrs['class'] = 'form-control'
             field.label = word_list[i]
             i += 1
-
-class UserCreateForm(UserCreationForm):
-    """ユーザー登録用フォーム"""
-
-    class Meta:
-        model = User
-        fields = (User.USERNAME_FIELD,)  # ユーザー名として扱っているフィールドだけ、作成時に入力する
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        for field in self.fields.values():
-            field.widget.attrs['class'] = 'form-control'
